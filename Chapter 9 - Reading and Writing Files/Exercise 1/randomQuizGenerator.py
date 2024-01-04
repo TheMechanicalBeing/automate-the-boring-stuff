7853ff75d87312db4fd6607f8220a3802085a687
TheMechanicalBeing <tornike.tsulukidze@gmail.com>
2024-01-04 23:56:25 +0400

    Started working on chapter 9

---
+++ Chapter 9 - Reading and Writing Files/Exercise 1/randomQuizGenerator.py
@@ -0,0 +1,31 @@
+import random
+
+capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
+            'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
+            'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
+            'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois':
+                'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas':
+                'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine':
+                'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan':
+                'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri':
+                'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada':
+                'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New Mexico': 'Santa Fe',
+            'New York': 'Albany',
+            'North Carolina': 'Raleigh', 'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
+            'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
+            'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee':
+                'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont':
+                'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West Virginia': 'Charleston',
+            'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
+
+for quizNum in range(35):
+    with open(f"quizzes/capitals_quiz{quizNum + 1}.txt", 'w') as quiz, \
+         open(f"answers/capitals_quiz_answers{quizNum + 1}", "w") as ans:
+
+        quiz.write("Name:\n\nDate:\n\nPeriod:\n\n")
+        quiz.write(f"State Capitals Quiz {quizNum + 1}".rjust(25, " ") + "\n\n")
+
+        states = list(capitals.keys())
+        random.shuffle(states)
+
+        quiz.write(str(states) + "\n\n")
---
+++ Chapter 9 - Reading and Writing Files/README.md
@@ -0,0 +1,27 @@
+# Chapter 9 - Reading and Writing Files
+
+## Exercise 1 - Generating Random Quiz Files
+
+Program that generates random quiz files. The quiz consists MCQ (multiple-choice questions) about US capitals.
+
+### Requirements
+- Creates 35 different quizzes
+- Creates 50 MCQ for each quiz, in random order.
+- Provides the correct answer and three random answers for each questions in random order
+- Writes the quizzes to 35 text files
+- Writes the answer keys to 35 text files
+- Store the states and their capitals in a dictionary
+- Call `open()` and `write()` for the quiz and answer key text files
+- Use `random.shuffle()` to randomize the order of the questions and multiplce-choice options
+
+## Exercise 2
+
+### Requirements
+
+## Exercise 3
+
+### Requirements
+
+## Exercise 4
+
+### Requirements
